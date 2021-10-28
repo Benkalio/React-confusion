0bd6457a100c34a510e256ae0475ca2a367defff
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2021-10-28 17:36:38 +0100

    Components 2

---
+++ src/shared/dishes.js
@@ -0,0 +1,38 @@
+export const DISHES = [
+    {
+        id: 0,
+        name: 'Pasta',
+        image: 'assets/images/image1.jpg',
+        category: 'mains',
+        label: 'Hot',
+        price: '4.99',
+        description: 'A unique combination of Nigerian delicacy.'
+    },
+    {
+        id: 1,
+        name: 'Beans',
+        image: 'assets/images/image1.jpg',
+        category: 'mains',
+        label: '',
+        price: '1.99',
+        description: 'A unique combination of Nigerian delicacy.'
+    },
+    {
+        id: 2,
+        name: 'Plantain',
+        image: 'assets/images/image1.jpg',
+        category: 'mains',
+        label: 'Hot',
+        price: '2.99',
+        description: 'A unique combination of Nigerian delicacy.'
+    },
+    {
+        id: 3,
+        name: 'Rice',
+        image: 'assets/images/image1.jpg',
+        category: 'mains',
+        label: '',
+        price: '3.99',
+        description: 'A unique combination of Nigerian delicacy.'
+    }
+]
