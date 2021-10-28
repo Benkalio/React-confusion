d0e8ea8c26a72fd923602bfa802efd78e4af2c06
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2021-10-28 18:04:27 +0100

    React Components 3

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Media} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardBody, CardText, CardTitle} from 'reactstrap';
 
 class Menu extends Component {
     constructor(props){
@@ -7,61 +7,47 @@ class Menu extends Component {
 
         //DEFINE STATE FOR THE RESTRAURANT MENU
         this.state = {
-            dishes: [
-                {
-                    id: 0,
-                    name: 'Pasta',
-                    image: 'assets/images/image1.jpg',
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '4.99',
-                    description: 'A unique combination of Nigerian delicacy.'
-                },
-                {
-                    id: 1,
-                    name: 'Beans',
-                    image: 'assets/images/image1.jpg',
-                    category: 'mains',
-                    label: '',
-                    price: '1.99',
-                    description: 'A unique combination of Nigerian delicacy.'
-                },
-                {
-                    id: 2,
-                    name: 'Plantain',
-                    image: 'assets/images/image1.jpg',
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '2.99',
-                    description: 'A unique combination of Nigerian delicacy.'
-                },
-                {
-                    id: 3,
-                    name: 'Rice',
-                    image: 'assets/images/image1.jpg',
-                    category: 'mains',
-                    label: '',
-                    price: '3.99',
-                    description: 'A unique combination of Nigerian delicacy.'
-                }
-            ]
+
+            //INTRODUCING THE ON CLICK PROPERTY ON THE MENU ITEMS, WHICH IS CODED ON THE CARD TAG BELOW
+            selectedDish: null
         }
     }
+
+    // WHAT HAPPENS WHEN THE DISH IS CLICKED, THIS CODE SELECTS THE STATE OF THE DISH
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        } else {
+            return (<div></div>)
+        }
+    }
+
+
     render(){
-        const menu =this.state.dishes.map((dish) => {
+        const menu =this.props.dishes.map((dish) => {
             return (
                 //CREATING 12 COLUMNS WITH BOOTSTRAP WITH THE KEY PROPERTY TO IDENTIFY THE LIST OF DISHES ABOVE
                 //USING REACT MEDIA CLASS TO RENDER THE APPLICATION ***READ UP REACT DOCUMENTATION ON MEDIA
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    {/* WHEN THE CARD IS CLICKED */}
+                    <Card onClick={() => this.onDishSelect()}>
+                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             )
         });
@@ -69,9 +55,10 @@ class Menu extends Component {
             //USING BOOTSTRAP CLASSES ##READ MORE ON CSS BOOTSTRAP
             <div className="container">
                 <div className="row">
-                    <Media list >
                         {menu}
-                    </Media>
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
