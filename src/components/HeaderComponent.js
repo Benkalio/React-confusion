a54ff434af72e9442a2ab03e878df2bb0c3734aa
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2021-12-01 23:39:18 +0100

    set

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,14 +4,58 @@ import { NavLink } from "react-router-dom";
 
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        }
+
+        // MAKE THIS AVAILABLE FOR US BELOW 
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
     render() {
         return (
             // USING REACT FRAGMENTS TO RETURN ELEMENTS (<React.Fr></React.Fr>)
             //GROUP REACT ELEMENT AND RETURN THEM DIRECTLY INTO THE DOM
             <React.Fragment>
-                <Navbar dark >
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"></span> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg"></span> About Us
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"></span> Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg"></span> Contact Us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>    
                     </div>
                 </Navbar>
                 <Jumbotron>
